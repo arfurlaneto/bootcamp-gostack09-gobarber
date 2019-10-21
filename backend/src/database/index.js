0e0bafb9766464dcf97d761140f38f71f380fdb1
arfurlaneto <arfurlaneto@gmail.com>
2019-10-21 01:24:35 -0300

    add back-end files

---
+++ backend/src/database/index.js
@@ -0,0 +1,34 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointments from '../app/models/Appointment';
+
+import databaseConfig from '../config/database';
+import mongoConfig from '../config/mongo';
+
+const models = [User, File, Appointments];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      mongoConfig.url,
+      { useNewUrlParser: true, useFindAndModify: true },
+    );
+  }
+}
+
+export default new Database();
