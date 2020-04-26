9849c6c364870534c61be33f11ed0a0bb83451d0
arfurlaneto <arfurlaneto@gmail.com>
2020-04-25 21:21:07 -0300

    Added background linear gradient.

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>SIGNIN</Text>
-    </View>
+    <Background>
+      <View>
+        <Text>SIGNIN</Text>
+      </View>
+    </Background>
   );
 }
