e5e5497d8faa76fb8fd11f19d17140f26d892cc9
arfurlaneto <arfurlaneto@gmail.com>
2020-04-26 18:30:28 -0300

    Pattern to make navigation available to sagas in mobile.

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import NavigationService from './services/navigation';
+
 import createRouter from './routes';
 
 const App = () => {
@@ -8,7 +10,11 @@ const App = () => {
 
   const Routes = createRouter(signed);
 
-  return <Routes />;
+  return (
+    <Routes
+      ref={(navigatorRef) => NavigationService.setNavigator(navigatorRef)}
+    />
+  );
 };
 
 export default App;
