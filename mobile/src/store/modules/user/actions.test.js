e70315c34973aa7467a10a65215ebae0e733c9ed
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-17 16:35:55 +0000

    add test

---
+++ mobile/src/store/modules/user/actions.test.js
@@ -0,0 +1,81 @@
+const actions = require("./actions")
+// @ponicode
+describe("actions.updateProfileRequest", () => {
+    test("0", () => {
+        let callFunction = () => {
+            actions.updateProfileRequest("data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20version%3D%221.1%22%20baseProfile%3D%22full%22%20width%3D%22undefined%22%20height%3D%22undefined%22%3E%3Crect%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22grey%22%2F%3E%3Ctext%20x%3D%22NaN%22%20y%3D%22NaN%22%20font-size%3D%2220%22%20alignment-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20fill%3D%22white%22%3Eundefinedxundefined%3C%2Ftext%3E%3C%2Fsvg%3E")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            actions.updateProfileRequest(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("actions.updateProfileSuccess", () => {
+    test("0", () => {
+        let callFunction = () => {
+            actions.updateProfileSuccess("user name")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            actions.updateProfileSuccess("user-name")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            actions.updateProfileSuccess("username")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            actions.updateProfileSuccess("user_name")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            actions.updateProfileSuccess("user123")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction = () => {
+            actions.updateProfileSuccess(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("actions.updateProfileFailure", () => {
+    test("0", () => {
+        let callFunction = () => {
+            actions.updateProfileFailure()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
